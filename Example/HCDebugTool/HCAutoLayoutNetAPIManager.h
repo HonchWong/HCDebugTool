0e81ade8541256e55e9a22304d07dc960a6da16c
HonchWong <honchwong404@gmail.com>
2019-01-14 11:59:07 +0800

    版本号0.1.8

---
+++ Example/HCDebugTool/HCAutoLayoutNetAPIManager.h
@@ -0,0 +1,17 @@
+//
+//  HCAutoLayoutNetAPIManager.h
+//  HCDebugTool_Example
+//
+//  Created by 黄鸿昌 on 2019/1/14.
+//  Copyright © 2019 honchwong404@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HCAutoLayoutNetAPIManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/HCDebugTool/HCAutoLayoutNetAPIManager.m
@@ -0,0 +1,13 @@
+//
+//  HCAutoLayoutNetAPIManager.m
+//  HCDebugTool_Example
+//
+//  Created by 黄鸿昌 on 2019/1/14.
+//  Copyright © 2019 honchwong404@gmail.com. All rights reserved.
+//
+
+#import "HCAutoLayoutNetAPIManager.h"
+
+@implementation HCAutoLayoutNetAPIManager
+
+@end
